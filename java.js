cf9754cf8682196dea42e512ba57a619fde678f9
Danilo <103371013+Danilo1144@users.noreply.github.com>
2022-05-04 16:20:26 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,44 @@
+const llamarAPI = document.getElementById("llamar")
+const nombre = document.querySelector("#nombre")
+const div = document.getElementById("poster")
+
+llamarAPI.addEventListener('click',CallAPI)
+
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com',
+		'X-RapidAPI-Key': '1befd4d5aemshee8b3d5a2c1816ep10cd0bjsnc26a7d22663b'
+	}
+};
+
+
+
+
+function CallAPI(){
+fetch(`https://online-movie-database.p.rapidapi.com/auto-complete?q=${nombre.value}`, options)
+	.then(response => response.json())
+	.then(data => {
+        const Array = data.d;
+        Array.map((element)=>{
+            console.log(element);
+            const titulo = element.l
+            const img = element.i.imageUrl
+            const acotres = element.s
+            const poster = `
+            <div>
+                <img src=${img}>
+                <h2>${titulo}</h2>
+                <p>${acotres}</p>
+            </div>
+                `
+            document.getElementById("carta").innerHTML += poster
+        })
+    })
+	.catch(err => console.error(err));
+
+}
+
+
+
